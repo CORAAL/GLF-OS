f0f89ea09ee62810b68fe9b1b54b9fb34c90572e
CORAAL <coraal@noreply.world>
2025-01-16 15:43:40 +0100

    add aliases glfos-systemInfo

--- modules/default/aliases.nix
+++ modules/default/aliases.nix
@@ -10,11 +10,31 @@
 
   config = lib.mkIf config.glf.aliases.enable {
 
-    environment.shellAliases = {
-      glf-update  = "sudo nix flake update --flake /etc/nixos";
-      glf-build = "nh os build /etc/nixos -H GLF-OS";
-      glf-switch = "nh os switch /etc/nixos -H GLF-OS";
-      glf-boot = "nh os boot /etc/nixos -H GLF-OS";
+    environment = {
+      systemPackages = with pkgs; [
+	pciutils
+	util-linux
+	inxi
+	gawk
+      ];
+      shellAliases = {
+        glf-update  = "sudo nix flake update --flake /etc/nixos";
+        glf-build = "nh os build /etc/nixos -H GLF-OS";
+        glf-switch = "nh os switch /etc/nixos -H GLF-OS";
+        glf-boot = "nh os boot /etc/nixos -H GLF-OS";
+	systeminfo = ''
+	    echo -e "\n--- Infos ---"; \
+	    inxi -M; \
+	    echo -e "\n--- CPU ---"; \
+	    lscpu | grep -E "(Architecture|CPU op-mode|Vendor ID|Model name|Mode\\(s\\) opératoire\\(s\\) des processeurs|Identifiant constructeur|Nom de modèle)" | awk "{print \$0}"; \
+	    echo -e "\n--- GPU ---"; \
+	    lspci | grep -E "VGA|3D"; \
+	    echo -e "\n--- Disque ---"; \
+	    df -h; \
+	    echo -e "\n--- RAM ---"; \
+	    free -h
+	'';
+      };
     };
     
   };
